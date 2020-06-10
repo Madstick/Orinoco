380e9f8435507e490dcb3c383623ce2653b967ab
Madstick <34131590+Madstick@users.noreply.github.com>
2020-06-10 15:52:31 +0200

    first

---
+++ src/produit.js
@@ -0,0 +1,9 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+console.log(id);
+
+ajax('http://localhost:3000/api/teddies/' + id ).then(function(data){
+console.log(data) 
+}).catch(error =>{console.log(error)});
+
+
