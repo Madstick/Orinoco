9f10b68912a3c46cebf5aeaeeed71044258ecf9d
Florian Roux <rouxflorian45@gmail.com>
2020-06-10 16:15:07 +0200

    ok

---
+++ src/ajax.js
@@ -0,0 +1,20 @@
+function ajax (url) {
+    return new Promise(function (resolve, reject) { 
+      const httpRequest = new XMLHttpRequest() 
+      httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === 4) {
+          if (httpRequest.status >= 200 && httpRequest.status < 300) { 
+            resolve(JSON.parse(httpRequest.responseText)) 
+          } else {
+            reject(httpRequest.status + " " + httpRequest.statusText) 
+          }
+        }
+      }
+      httpRequest.onerror = function(error){
+        reject(error)
+      }
+      httpRequest.open('GET', url, true) 
+      httpRequest.send() 
+    })
+  }
+  
---
+++ src/app.js
@@ -0,0 +1,35 @@
+ajax('http://localhost:3000/api/teddies').then(function(data){
+  console.log(data)
+  // data.forEach(TeddyDiv => buildTeddyDiv(TeddyDiv));
+  const container = document.getElementById('products')
+  for(let teddyInfo of data){
+  buildTeddyDiv(teddyInfo,container);
+ } 
+}).catch(error =>{console.log(error)});
+ 
+
+const buildTeddyDiv = (TeddyDiv,container) => {
+  // Créer des éléments nécessaires pour construire une carte des produit
+  const div = document.createElement("div");
+  const h3 = document.createElement("h3");
+  const h4 = document.createElement("h4");
+  const a = document.createElement("a");
+  const img = document.createElement("img");
+  // Ajouter les éléments nouvellement créés dans le DOM
+  container.append(div);
+  h3.append(a);
+  div.append(h3);
+  div.append(img);
+  div.append(h4);
+  // Définir le contenu et les attributs
+  a.innerHTML = TeddyDiv.name;
+  a.setAttribute("href", "produit.html?id=" + TeddyDiv._id);
+  img.setAttribute("src", TeddyDiv.imageUrl);
+  h4.innerHTML = TeddyDiv.price;
+  div.setAttribute("class", "card");
+};
+
+
+
+
+
