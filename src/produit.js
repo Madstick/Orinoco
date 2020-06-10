c53084e8770ed54cf1f5a31e882469ebb44eef19
Florian Roux <rouxflorian45@gmail.com>
2020-06-10 16:29:16 +0200

    fixx

--- src/produit.js
+++ src/produit.js
@@ -6,13 +6,3 @@ ajax('http://localhost:3000/api/teddies/' + id ).then(function(data){
 console.log(data) 
 }).catch(error =>{console.log(error)});
 
-
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get('id');
-console.log(id);
-
-ajax('http://localhost:3000/api/teddies/' + id ).then(function(data){
-console.log(data) 
-}).catch(error =>{console.log(error)});
-
-
