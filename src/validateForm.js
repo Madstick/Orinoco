1fd11f35f8403f72533ce44020bfb969a72619c6
Florian Roux <rouxflorian45@gmail.com>
2020-07-12 22:55:28 +0200

    quick fix validation

--- src/validateForm.js
+++ src/validateForm.js
@@ -3,24 +3,24 @@ function validateForm(){ // Création d'une fonction validateform
         alert("Votre panier est vide"); // On crée une une alerte
         return false;   // On retourne le boléen "false" pour indiquer que ce n'est pas valide
     }
-    var form1 = document.forms["myForm"]["firstname"].value; // Création d'une variable qui prend en compte la valeur de "firstname" dans le formulaire "myForm"
-    if (form1 ==""){ // Si l'input "firstname" est vide
-        alert("La case Prénom doit être remplie"); // On crée une alerte
-        return false; // On retourne false
-    }
-    var form2 = document.forms["myForm"]["lastname"].value;
-    if (form2 ==""){
-        alert("La case Nom doit être remplie");
+    var form1 = document.forms["myForm"]["firstname"]; // Création d'une variable qui prend en compte la valeur de "firstname" dans le formulaire "myForm"
+    if (!form1.checkValidity()){ // Si le champ n'est pas valide pour un e-mail
+        alert("Il y'a une erreur sur le champ prénom")
         return false;
     }
-    var form3 = document.forms["myForm"]["address"].value;
-    if (form3 ==""){
-        alert("La case Adresse doit être remplie");
+    var form2 = document.forms["myForm"]["lastname"];
+    if (!form2.checkValidity()){ // Si le champ n'est pas valide pour un e-mail
+        alert("Il y'a une erreur sur le champ de nom")
         return false;
     }
-    var form4 = document.forms["myForm"]["city"].value;
-    if (form4 ==""){
-        alert("La case Ville doit être remplie");
+    var form3 = document.forms["myForm"]["address"];
+    if (!form3.checkValidity()){ // Si le champ n'est pas valide pour un e-mail
+        alert("Il y'a une erreur sur le champ d'adresse")
+        return false;
+    }
+    var form4 = document.forms["myForm"]["city"];
+    if (!form4.checkValidity()){ // Si le champ n'est pas valide pour un e-mail
+        alert("Il y'a une erreur sur le champ ville")
         return false;
     }
     var form5 = document.forms["myForm"]["date"].value;
